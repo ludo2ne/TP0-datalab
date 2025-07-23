185bd604b4edf0ed8a7480d8d3e4fca5e2d20b4b
Deneuville Ludovic <92184287+ludo2ne@users.noreply.github.com>
2025-07-23 16:00:08 +0200

    Update testinit.sh

--- testinit.sh
+++ testinit.sh
@@ -1,2 +1,9 @@
+#!/bin/sh
+
+# Install packages and VSCode extensions
+# Expected parameters : None
+
+pip install inquirerPy psycopg2-binary pytest python-dotenv tabulate
+
 code-server --install-extension charliermarsh.ruff
-pip install pytest
+code-server --uninstall-extension ms-python.flake8
